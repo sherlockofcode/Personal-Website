e0dc90313bec55bfd47aeba6562d7086f0098d0e
Shreya Terupally <terupallyshreya@gmail.com>
2018-01-29 14:22:01 +0530

    github website

---
+++ Desktop/shreya.terupally/itws/html/HTMLProject/contact.js
@@ -0,0 +1,14 @@
+function hidecontent1( ){
+var i;
+var x=document.getElementsByClassName("content1");
+for(i=0;i<x.length;i++){
+x[i].style.display= "block";
+}
+}
+function hide(){
+var i;
+var x=document.getElementsByClassName("content");
+for(i=0;i<x.length;i++){
+x[i].style.display= "block";
+}
+}
